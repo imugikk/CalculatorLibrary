adcd7cef7d5af8d596aee5d828cf6edf72f1b420
ugik <ugyparaptughessa@gmail.com>
2023-07-18 10:42:50 +0700

    add func

--- Sources/CalculatorLibrary/CalculatorLibrary.swift
+++ Sources/CalculatorLibrary/CalculatorLibrary.swift
@@ -1,6 +1,23 @@
-public struct CalculatorLibrary {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public class Calculator {
+    
+    public init() {}
+    
+    public func add(valueA: Int, valueB: Int) -> Int {
+        return valueA + valueB
     }
+    
+    public func subtract(valueA: Int, valueB: Int) -> Int {
+        return valueA - valueB
+    }
+    
+    public func multiply(valueA: Int, valueB: Int) -> Int {
+        return valueA * valueB
+    }
+    
+    public func divide(valueToDivide: Int, divisor: Int) -> Int {
+        return valueToDivide / divisor
+    }
+    
 }
--- Sources/CalculatorLibraryViews/SwiftUIView.swift
+++ Sources/CalculatorLibraryViews/SwiftUIView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 public struct SwiftUIView: View {
     public init() {}
+    @available(macOS 10.15, *)
     @available(iOS 13.0, *)
     public var body: some View {
         Text("This is from package manager!")
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+stru@available(macOS 10.15, *)
+ct SwiftUIView_Previews: PreviewProvider {
     @available(iOS 13.0, *)
     static var previews: some View {
         SwiftUIView()
